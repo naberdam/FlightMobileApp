18eea46caab1423690acbf6c6fa79fc188cded8d
TalZig <50695642+TalZig@users.noreply.github.com>
2020-06-07 14:18:32 +0300

    for yoel

--- app/src/main/java/com/example/flightmobileapp/GameActivity.kt
+++ app/src/main/java/com/example/flightmobileapp/GameActivity.kt
@@ -2,12 +2,10 @@ package com.example.flightmobileapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_game)
     }
 }
