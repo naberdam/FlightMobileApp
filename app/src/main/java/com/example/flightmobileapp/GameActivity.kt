7b9706a3ee4f6032ca9d3bab047a0f6c4af05515
TalZig <50695642+TalZig@users.noreply.github.com>
2020-06-09 16:52:48 +0300

    for me

--- app/src/main/java/com/example/flightmobileapp/GameActivity.kt
+++ app/src/main/java/com/example/flightmobileapp/GameActivity.kt
@@ -1,31 +1,36 @@
 package com.example.flightmobileapp
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.drawable.ShapeDrawable
-import android.graphics.drawable.shapes.RectShape
 //import android.support.v8.1.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
-import android.graphics.Bitmap
-import android.graphics.drawable.BitmapDrawable
-import android.graphics.drawable.shapes.OvalShape
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 
-class GameActivity : AppCompatActivity() {
+
+import JoystickView
+import JoystickView.JoystickListener
+import android.os.Bundle
+//import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+
+class GameActivity : AppCompatActivity(), JoystickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_game)
-/*        val bitmap: Bitmap = Bitmap.createBitmap(700, 1000, Bitmap.Config.ARGB_8888)
-        val canvas: Canvas = Canvas(bitmap)
-        var left = 200
-        var top = 400
-        var right = 600
-        var bottom = 800
-        var shapeDrawable:ShapeDrawable
-        shapeDrawable = ShapeDrawable(OvalShape())
-        shapeDrawable.setBounds(left,top,right,bottom)
-        shapeDrawable.getPaint().setColor(Color.parseColor("#009191"))
-        shapeDrawable.draw(canvas)*/
+        val joystick = JoystickView(this)
+        setContentView(R.layout.activity_main)
+    }
 
+    override fun onJoystickMoved(
+        xPercent: Float,
+        yPercent: Float,
+        id: Int
+    ) {
+        when (id) {
+            R.id.joystickRight -> Log.d(
+                "Right Joystick",
+                "X percent: $xPercent Y percent: $yPercent"
+            )
+            R.id.joystickLeft -> Log.d(
+                "Left Joystick",
+                "X percent: $xPercent Y percent: $yPercent"
+            )
+        }
     }
 }
