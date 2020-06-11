0ec11509e6aa267e0b4f20c48aac1d840124f2b3
TalZig <50695642+TalZig@users.noreply.github.com>
2020-06-12 02:19:28 +0300

    before joystick values x and y work

--- app/src/main/java/com/example/flightmobileapp/GameActivity.kt
+++ app/src/main/java/com/example/flightmobileapp/GameActivity.kt
@@ -2,34 +2,31 @@ package com.example.flightmobileapp
 //import android.support.v8.1.app.AppCompatActivity
 
 
-import com.example.flightmobileapp.JoystickView.JoystickListener
 import android.os.Bundle
 //import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import io.github.controlwear.virtual.joystick.android.JoystickView
+import kotlinx.android.synthetic.main.activity_game.*
+import java.lang.Math.*
 
 
-class GameActivity : AppCompatActivity(), JoystickListener {
+class GameActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val joystick = JoystickView(this)
-        setContentView(R.layout.activity_game)
-    }
-
-    override fun onJoystickMoved(
-        xPercent: Float,
-        yPercent: Float,
-        id: Int
-    ) {
-        when (id) {
-            R.id.joystickRight -> Log.d(
-                "Right Joystick",
-                "X percent: $xPercent Y percent: $yPercent"
-            )
-            R.id.joystickLeft -> Log.d(
-                "Left Joystick",
-                "X percent: $xPercent Y percent: $yPercent"
-            )
-        }
+        setContentView(R.layout.activity_main)
+        val joystick = findViewById(R.id.joystick) as JoystickView
+        joystick.setOnMoveListener(object : JoystickView.OnMoveListener {
+            override fun onMove(angle: Int, strength: Int) {
+                val rad = toRadians(angle + 0.0)
+                var x = kotlin.math.cos(rad)
+                var y = kotlin.math.sin(rad)
+                x = (x * strength) / 100
+                y = (y * strength) / 100
+                print(x)
+                print(y)
+            }
+        })
     }
 }
