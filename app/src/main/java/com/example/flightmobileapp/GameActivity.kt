0ab4d9efdcf57306c85cc291aee4355e54b4d52b
TalZig <50695642+TalZig@users.noreply.github.com>
2020-06-14 10:38:12 +0300

    before joystick values x and y work

--- app/src/main/java/com/example/flightmobileapp/GameActivity.kt
+++ app/src/main/java/com/example/flightmobileapp/GameActivity.kt
@@ -2,12 +2,21 @@ package com.example.flightmobileapp
 //import android.support.v8.1.app.AppCompatActivity
 
 
+import Api
+import android.graphics.BitmapFactory
 import android.os.Bundle
 //import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.GsonBuilder
 import io.github.controlwear.virtual.joystick.android.JoystickView
 import kotlinx.android.synthetic.main.activity_game.*
+import okhttp3.ResponseBody
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import java.lang.Math.*
 
 
@@ -15,10 +24,34 @@ class GameActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val joystick = findViewById(R.id.joystick) as JoystickView
-        joystick.setOnMoveListener(object : JoystickView.OnMoveListener {
+        val gson = GsonBuilder()
+            .setLenient()
+            .create()
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://10.0.2.2:8686/")
+            .addConverterFactory(GsonConverterFactory.create(gson)).build()
+        val api = retrofit.create(Api::class.java)
+        val body = api.getImg().enqueue(object : Callback<ResponseBody> {
+            override fun onResponse(
+                call: Call<ResponseBody>,
+                response: Response<ResponseBody>
+            ) {
+                val I = response.body()?.byteStream()
+                val B = BitmapFactory.decodeStream(I)
+                runOnUiThread {
+                    SimulatorView.setImageBitmap(B)
+                }
+            }
+
+            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                print("basa")
+            }
+        })
+        setContentView(R.layout.activity_game)
+        //val joystick = findViewById(R.id.joystick) as JoystickView
+/*        joystick.setOnMoveListener(object : JoystickView.OnMoveListener {
             override fun onMove(angle: Int, strength: Int) {
+
                 val rad = toRadians(angle + 0.0)
                 var x = kotlin.math.cos(rad)
                 var y = kotlin.math.sin(rad)
@@ -27,6 +60,6 @@ class GameActivity : AppCompatActivity() {
                 print(x)
                 print(y)
             }
-        })
+        })*/
     }
 }
--- app/src/main/java/com/example/flightmobileapp/MainActivity.kt
+++ app/src/main/java/com/example/flightmobileapp/MainActivity.kt
@@ -4,11 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.gson.GsonBuilder
 import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val gson = GsonBuilder()
+            .setLenient()
+            .create()
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://10.0.2.2:8686/")
+            .addConverterFactory(GsonConverterFactory.create(gson))
         setContentView(R.layout.activity_main)
 
         val url = findViewById<Button>(R.id.URL)
